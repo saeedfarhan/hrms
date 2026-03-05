0667483e59efad426e0c0ca3ed75a2d89ba82c6c
saeedfarhan <saeedfarhan129@gmail.com>
2026-03-05 17:48:31 +0530

    db url from env

--- backend/app/db.py
+++ backend/app/db.py
@@ -1,9 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:tzfcBhcoyxoizdTmZNLADXlrnPxftVtk@trolley.proxy.rlwy.net:56713/railway"
+
+SQLALCHEMY_DATABASE_URL = os.environ.get('DATABASE_URL')
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
